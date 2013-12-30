ba0c24f09c8b44bcb4756ff67cb2cb5b7dcaf6fa
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2013-12-30 17:11:18 -0500

    Moved around the JS files.

---
+++ lib/widgets.php
@@ -0,0 +1,9 @@
+<?php
+
+register_sidebar(array(
+  'name' => __( 'Right Hand Sidebar' ),
+  'id' => 'right-sidebar',
+  'description' => __( 'Widgets in this area will be shown on the right-hand side.' ),
+  'before_title' => '<h1>',
+  'after_title' => '</h1>'
+));
