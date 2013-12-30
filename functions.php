bb8c21e530ee4d9a713016eb190ecd5dd53dc9c4
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2013-12-30 17:13:03 -0500

    Moved around & introduced PHP files.

--- functions.php
+++ functions.php
@@ -1,18 +1,12 @@
 <?php
-add_action ( 'init', 'oor_enqueue_scripts' );
-function oor_enqueue_scripts(){
-	if ( is_admin() ) return;
-	wp_enqueue_style( 'default', get_stylesheet_uri() );
-	wp_enqueue_style( 'fonts', 'http://fonts.googleapis.com/css?family=Alegreya:400italic,700italic,900italic,400,700,900|Ruda:400,900,700' );
-	wp_enqueue_script( 'jquery', null, null, null, true );
-	wp_enqueue_script( 'html5shiv', get_stylesheet_directory_uri() . '/js/html5shiv.js' );
-	wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/javascript.php', null, null, true );
+
+require 'lib/templating.php';
+require 'lib/widgets.php';
+
+if ( ! is_admin() ) {
+  add_action( 'init', 'oor_enqueue_scripts' );
 }
 
-register_sidebar(array(
-  'name' => __( 'Right Hand Sidebar' ),
-  'id' => 'right-sidebar',
-  'description' => __( 'Widgets in this area will be shown on the right-hand side.' ),
-  'before_title' => '<h1>',
-  'after_title' => '</h1>'
-));
+remove_action( 'wp_head', 'wp_generator' ); 
+remove_action( 'wp_head', 'wlwmanifest_link' ); 
+remove_action( 'wp_head', 'rsd_link' );
