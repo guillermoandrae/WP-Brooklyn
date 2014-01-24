4db66b9a893920b660b33a23f641cb0e2464699e
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2014-01-23 23:03:48 -0500

    Added relative-nav class.

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 get_header();
 get_template_part( 'loop', 'singular' );
 ?>
-    <span class="previous-nav"><?php previous_posts_link( '<i class="fa fa-chevron-circle-left fa-4x"></i>' ); ?></span>
-    &nbsp;
-    <span class="next-nav"><?php next_posts_link( '<i class="fa fa-chevron-circle-right fa-4x"></i>' ); ?></span>
+    <footer class="relative-nav">
+        <span class="previous-nav"><?php previous_posts_link( '<i class="fa fa-chevron-circle-left fa-4x"></i>' ); ?></span>
+        &nbsp;
+        <span class="next-nav"><?php next_posts_link( '<i class="fa fa-chevron-circle-right fa-4x"></i>' ); ?></span>
+    </footer>
 <?php get_footer(); ?>
