bb1efc9293896f70c94634a90afeae1127ab4486
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2014-01-26 23:17:17 -0500

    Standards fixes.

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 <head>
 <title><?php bloginfo( 'name' ); ?><?php wp_title( ' | ' ); ?></title>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="<?php bloginfo( 'description' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
