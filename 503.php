f6d86d3417c1536bb7c0c6bcb63da976eaaa2769
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2014-10-30 23:07:30 -0400

    Added some bait to the maintenance page.

--- 503.php
+++ 503.php
@@ -1,1 +1,8 @@
-<pre>tweaking... come back later</pre>
+<pre>
+    tweaking... come back later
+
+    in the meantime, check out:
+    <a href="https://github.com/guillermoandrae">https://github.com/guillermoandrae</a>
+    <a href="https://twitter.com/guillermoandrae">https://twitter.com/guillermoandrae</a>
+    <a href="https://linkedin.com/in/guillermoandrae">https://linkedin.com/in/guillermoandrae</a>
+</pre>
