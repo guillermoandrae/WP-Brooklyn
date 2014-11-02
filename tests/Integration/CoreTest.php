38b6c7c91fca1457ce8eb39218a2a57554e172ff
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2014-11-02 01:58:55 -0400

    Added foundation for testing.

---
+++ tests/Integration/CoreTest.php
@@ -0,0 +1,8 @@
+<?php
+class CoreTest extends WP_UnitTestCase
+{
+    public function testCurrentThemeIsActive()
+    {
+        $this->assertEquals('wp-skeleton-theme', get_option('current_theme'));
+    }
+}
